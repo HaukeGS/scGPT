f3e2f27511adae8afb17d7b0978139dbb240e0e5
HaukeGS <schuele.hauke@gmail.com>
2025-12-03 10:03:37 +0100

    training configuration for large model

--- pretrain_distributed.sh
+++ pretrain_distributed.sh
@@ -3,18 +3,18 @@
 # See `man sbatch` or https://slurm.schedmd.com/sbatch.html for descriptions of sbatch options.
 #SBATCH --job-name=scGPT_pretrain              # Gets overwritten by run_and_monitor_job.sh!
 #SBATCH --nodes=1                                     # Number of nodes to request
-#SBATCH --ntasks-per-node=4                           # total number of tasks per node
+#SBATCH --ntasks-per-node=8                           # total number of tasks per node
 #SBATCH --cpus-per-task=4                             # Number of CPUs to request
-#SBATCH --gres=gpu:a100:4                             # Number of GPUs to request
+#SBATCH --gres=gpu:a100:8                             # Number of GPUs to request
 #SBATCH --mem-per-gpu=4GB
 #SBATCH --partition=ampere
 #SBATCH --output=/home/hauke.schuele/scGPT_distributed/logs/%x-%j.out  # File to which STDOUT will be written
 #SBATCH --error=/home/hauke.schuele/scGPT_distributed/logs/%x-%j.err   # File to which STDERR will be written
-#SBATCH --time=00:15:00              # Time limit (hh:mm:ss) debug time
-echo ""
-#SBATCH --mail-user=hauke.schuele@stud.uni-hannover.de
+#SBATCH --time=20-00:00:00              # Time limit (hh:mm:ss) production time
+#SBATCH --mail-user=schuele.hauke@gmail.com
 #SBATCH --mail-type=ALL       # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --time=10-00:00:00              # Time limit (hh:mm:ss) production time
+echo ""
+#SBATCH --time=00:15:00              # Time limit (hh:mm:ss) debug time
 
 module load mamba
 # micromamba activate scgpt_stateful
@@ -82,12 +82,12 @@ fi
 srun python -u scGPT_distributed/pretrain_distributed_args.py \
     --tissues "${TISSUES[@]}" \
     --data-tissue-path "$DATA_TISSUE_PATH" \
-    --epochs 2 \
+    --epochs 6 \
     --training-tasks "both" \
     --save-dir ./save/pretrain-distributed-[$SLURM_JOB_ID]-$(date +%Y-%m-%d_%H-%M-%S) \
     --vocab-path "/data/datasets/biology/scGPT-data/preprocessed/default_census_vocab.json" \
-    --save-interval 10000 \
-    --log-interval 250 \
+    --save-interval 50000 \
+    --log-interval 1000 \
     --batch-size 24 \
     --valid-ratio 0.04 \
     --trunc-by-sample \
